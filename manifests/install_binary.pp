226c1624c886058828852eed99617967beea8fda
William Hefter <william@whefter.de>
2017-04-14 11:59:38 +0200

    Add caching for download URL determination and handling for failure to determine a URL.

--- manifests/install_binary.pp
+++ manifests/install_binary.pp
@@ -22,7 +22,7 @@ define syncthing::install_binary
   $download_url = strip(template('syncthing/download_url.erb'))
   $basename = regsubst(inline_template('<%- require "uri" -%><%= File.basename(URI.parse(@download_url).path) %>'), '(-v\d+\.\d+\.\d+)[\w\.]+$', '\1', '')
 
-#  notify { $download_url: }
+#  notify { "${path}_${download_url}": message => length($download_url)}
 
   exec { "create binary folder for ${name}":
     command => "mkdir -p \"${path}\"",
@@ -37,20 +37,19 @@ define syncthing::install_binary
     group  => $group,
     mode   => '0770',
   }
-  ->
-  exec { "download and unpack syncthing for ${name}":
-    cwd     => $path,
-    path    => $::path,
-    user    => $daemon_uid,
-    command => "wget -O - ${download_url} | tar xzf - --strip-components=1",
-#    creates => "${path}/${basename}",
-    creates => "${path}/syncthing",
+  
+  if length($download_url) > 0 {
+    exec { "download and unpack syncthing for ${name}":
+      cwd     => $path,
+      path    => $::path,
+      user    => $daemon_uid,
+      command => "wget -O - ${download_url} | tar xzf - --strip-components=1",
+      creates => "${path}/syncthing",
+      require => [
+        File[$path],
+      ],
+    }
+  } else {
+    notify { "No download URL returned for syncthing binary for path ${path}. Perhaps the requested version is too old or there was an issue with the request to Github.": }
   }
-#   ->
-#  file { "${path}/syncthing":
-#    ensure => link,
-#    owner  => $user,
-#    group  => $group,
-#    target => "${path}/${basename}/syncthing",
-#  }
 }
